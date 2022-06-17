42fd4bf0c5fe5e97afe99fc1ca190c4d2c6089fc
CobyBoy <aboutacoby@gmail.com>
2022-06-17 07:40:46 -0300

    Added type

--- src/components/ColumnHeader.tsx
+++ src/components/ColumnHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 //@ts-ignore
 import { getColumnLabel } from '../shared/utils.ts'
 
-const ColumnHeader = ({colIndex}) => {
+const ColumnHeader = ({colIndex}: { colIndex: number}) => {
   return (
     <td key={colIndex} className="column-header">{getColumnLabel(colIndex - 1)}</td> //ABCD col
   )
